af2760d3a3e7da620978a562c7e4402a1bca2c68
kauagabr <kaua32475@gmail.com>
2025-05-05 22:44:53 -0300

    Update: Atualização nas telas principais

--- huoc-front/src/app/researcherPage/page.tsx
+++ huoc-front/src/app/researcherPage/page.tsx
@@ -1,84 +1,75 @@
-'use client';
+"use client"; 
 
-import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
+import dynamic from "next/dynamic";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
-export default function NurseHome() {
-  const [userName, setUserName] = useState("");
-  const router = useRouter();
+const DashboardCharts = dynamic(() => import('../../components/dashboard/Dashboard'), {
+  ssr: false,
+});
 
-  useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    if (storedUser) {
-      try {
-        const parsedUser = JSON.parse(storedUser);
-        setUserName(parsedUser.name || "Usuário");
-      } catch {
-        setUserName("Usuário");
+export default function ResearcherHome() {
+    const [userName, setUserName] = useState("");
+    const router = useRouter();
+  
+    useEffect(() => {
+      const storedUser = localStorage.getItem("user");
+      if (storedUser) {
+        try {
+          const parsedUser = JSON.parse(storedUser);
+          setUserName(parsedUser.name || "Usuário");
+        } catch {
+          setUserName("Usuário");
+        }
       }
-    }
-  }, []);
+    }, []);
 
-  const handleNavigateToForm = () => {
-    router.push("/researcherPage/centralResearcher");
-  };
+    const handleLogout = () => {
+        localStorage.removeItem('token'); 
 
-  const handleLogout = () => {
-    localStorage.removeItem('token');
+        router.push('/login');
+    };
 
-    router.push('/login');
-  };
+    return (
+      <div className="bg-gray-50 bg-cover bg-bottom" > 
+        {/* Navbar */}
+        <header className="bg-[#BFDBFE] flex items-center justify-between px-6 py-4 shadow-md">
+          <div className="flex items-center gap-3">
+            <Image src="/huoc-system.png" alt="Logo HUOC" width={40} height={40} />
+            <h1 className="text-lg font-semibold text-gray-800">
+              HUOC - Sistema de Coleta de Dados Clínicos
+            </h1>
+          </div>
 
-  return (
-    <div className="flex flex-col min-h-screen">
+          {/* Botões do lado direito */}
+          <div className="flex items-center gap-3">
+            <button
+              onClick={() => router.push('/researcherPage/data-export')}
+              className="text-blue-700 mr-3 font-semibold flex items-center gap-1 transition-all transform hover:scale-105 cursor-pointer border-b-2 border-transparent hover:border-blue-700"
+              title="Exportar Dados">
+              <span>Exportar Dados</span>
+            </button>
 
-      {/* Navbar */}
-      <header className="bg-[#BFDBFE] flex items-center justify-between px-6 py-4 shadow-md">
-        <div className="flex items-center gap-3">
-          <Image src="/huoc-system.png" alt="Logo HUOC" width={40} height={40} />
-          <h1 className="text-lg font-semibold text-gray-800">HUOC - Sistema de Coleta de Dados Clínicos</h1>
-        </div>
-
-        <button
-          onClick={handleLogout}
-          className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded shadow transition-all transform hover:scale-105 cursor-pointer"
-          title="Sair"
-        >
-          Sair
-        </button>
-      </header>
-
-      <main
-        className="flex-grow flex flex-col items-center justify-center bg-cover bg-bottom px-4"
-        style={{ backgroundImage: "url('/bg.jpg')" }}
-      >
-        <div className="bg-white bg-opacity-60 rounded-2xl shadow-xl p-10 max-w-3xl w-full text-center">
-          <h1 className="text-3xl font-bold text-gray-800 mb-4">
-            Bem-vindo à Área do Pesquisador
-          </h1>
-          <p className="text-lg text-gray-700 mb-8">
-            Você está acessando o Sistema HUOC - Coleta de Dados Clínicos.
-          </p>
-
-          <button
-            onClick={() => router.push("/researcherPage/centralResearcher")}
-            className="bg-blue-600 hover:bg-blue-700 text-white text-lg font-semibold py-4 px-8 rounded-full shadow-lg transition-all transform hover:scale-105 cursor-pointer"
-          >
-
-            Visualizar Dados dos Pacientes
-          </button>
-
-        </div>
+            <button
+              onClick={handleLogout}
+              className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded shadow transition"
+              title="Sair">
+              Sair
+            </button>
+          </div>
+        </header>
 
 
-      </main>
+        {/* Main */}
+        <main className="flex flex-col items-center p-10 justify-center py-6 bg-gray-50 min-h-screen bg-cover bg-bottom" style={{ backgroundImage: "url('/bg.jpg')" }}>
+            <DashboardCharts />
+        </main>
 
-
-      {/* Footer */}
-      <footer className="bg-[#BFDBFE] text-center py-4 text-gray-800 text-sm">
-        © 2025 Sistema HUOC. Todos os direitos reservados.
-      </footer>
-    </div>
-  );
+        {/* Footer */}
+        <footer className="bg-[#BFDBFE] text-center py-4 text-sm text-gray-800">
+            © 2025 Sistema HUOC. Todos os direitos reservados.
+        </footer>
+      </div>
+    );
 }
